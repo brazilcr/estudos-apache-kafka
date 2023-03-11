9c7a1a031dc0c7c6c555da61966185c4fe0faddd
brazilcr <baimaclaudio@gmail.com>
2023-03-11 18:28:17 -0300

    added callback function

---
+++ str-producer/src/main/java/com/baima/strproducer/resources/StringProducerResource.java
@@ -0,0 +1,26 @@
+package com.baima.strproducer.resources;
+
+import com.baima.strproducer.services.StringProducerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/producer")
+public class StringProducerResource {
+
+    private final StringProducerService producerService;
+
+    public StringProducerResource(StringProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
