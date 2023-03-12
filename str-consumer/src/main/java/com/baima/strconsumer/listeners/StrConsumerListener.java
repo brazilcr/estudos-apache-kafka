4318d9875dcf6a701907cb2674b38bae86c2d4c6
brazilcr <baimaclaudio@gmail.com>
2023-03-12 13:57:50 -0300

    str consumer

--- str-consumer/src/main/java/com/baima/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/baima/strconsumer/listeners/StrConsumerListener.java
@@ -1,2 +1,24 @@
-package com.baima.strconsumer.listeners;public class StrConsumerListener {
+package com.baima.strconsumer.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+@Log4j2
+@Component
+public class StrConsumerListener {
+    @KafkaListener( groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void create(String message) {
+        log.info("CREATE :: Received message {}", message
+        );
+    }
+
+    @KafkaListener( groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message) {
+        log.info("LOG :: Received message {}", message);
+    }
+
+    @KafkaListener( groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY :: Received message {}", message);
+    }
 }
---
+++ str-consumer/src/test/java/com/baima/strconsumer/StrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baima.strconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
