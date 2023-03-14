d35d759753543b980397972e580a11a04848c732
brazilcr <baimaclaudio@gmail.com>
2023-03-13 21:34:41 -0300

    added producer config

--- payment-service/src/main/java/com/baima/paymentservice/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/baima/paymentservice/impl/PaymentServiceImpl.java
@@ -2,13 +2,26 @@ package com.baima.paymentservice.impl;
 
 import com.baima.paymentservice.model.Payment;
 import com.baima.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serial;
+import java.io.Serializable;
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL :: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+        log.info("enviando pagamento ...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
